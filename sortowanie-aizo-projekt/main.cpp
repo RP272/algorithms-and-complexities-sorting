097134ab1f31f84f0b3b69c4dbaa608ff8ce27d9
robloxDigital <107726289+robloxDigital@users.noreply.github.com>
2024-04-25 09:31:44 +0200

    Fin

--- sortowanie-aizo-projekt/main.cpp
+++ sortowanie-aizo-projekt/main.cpp
@@ -9,5 +9,8 @@ int main()
     DataType datatype = DataTypePicker::pick();
     ActionType actiontype = ActionTypePicker::pick();
     ActionDataBundler::bundle(datatype, actiontype);
+    std::cin.ignore(1);
+    std::cout << "Nacisnij dowolny klawisz aby zakonczyc program." << std::endl;
+    std::cin.get();
     return 0;
 }
